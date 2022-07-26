c4638e8eb510bc5f94bbaad551d5beaf7dcfe5d4
Sstark97 <aitorscinfo@gmail.com>
2022-07-26 13:17:16 +0100

    Add loadEnv in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
   const generalConfig = {
     plugins: [react()],
   }
 
   if (command === 'serve' && mode === "development") {
-    console.log('serve development')
     return {
       ...generalConfig,
       server: {
-        port: 3000,
+        port: parseInt(env.VITE_PORT),
       }
     }
-    
+
   } else {
     return generalConfig
   }
