a314966d16f4ba6390105ce0e71fbc326ef6dcdc
Sstark97 <aitorscinfo@gmail.com>
2022-07-26 13:00:26 +0100

    Modify vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ command, mode }) => {
+  const generalConfig = {
+    plugins: [react()],
+  }
+
+  if (command === 'serve' && mode === "development") {
+    console.log('serve development')
+    return {
+      ...generalConfig,
+      server: {
+        port: 3000,
+      }
+    }
+    
+  } else {
+    return generalConfig
+  }
+  
 })
