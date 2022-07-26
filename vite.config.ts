00dc3c83d6c175bb93331ba17b325bac423e579b
Sstark97 <aitorscinfo@gmail.com>
2022-07-26 13:24:53 +0100

    Add alias config in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+const getAlliases = () => {
+  return {
+    '@assets': resolve(__dirname, './src/assets'),
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd())
   const generalConfig = {
     plugins: [react()],
+    resolve: {
+      alias: getAlliases(),
+    }
   }
 
   if (command === 'serve' && mode === "development") {
