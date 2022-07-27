9a16cf4b902ce23810ca2ad2c6ee0038a4c16079
Sstark97 <aitorscinfo@gmail.com>
2022-07-27 14:17:57 +0100

    Testing PortFolio API

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
-const Home = () => (
-    <div className="home">
-      <p>Portfolio Home!</p>
-    </div>
-)
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default Home
+const Home = () => {
+
+  const [data, setData] = useState<any>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userData: any = await axios.get(`${import.meta.env.VITE_API}api/v1/user`, {
+        headers: {
+          'x-access-tokens': import.meta.env.VITE_API_KEY
+        }
+      });
+
+      setData(userData.data);
+    }
+
+    fetchData();
+  }), [];
+
+  return (
+    <div className="home">
+      <p>{data !== undefined && data.presentation}</p>
+    </div>
+  );
+};
+
+export default Home;
