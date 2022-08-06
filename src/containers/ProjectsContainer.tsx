3347c90ad3a3375c5c7b3d579c5c6288cacc876d
Sstark97 <aitorscinfo@gmail.com>
2022-08-06 11:30:03 +0100

    Add Projects Page

---
+++ src/containers/ProjectsContainer.tsx
@@ -0,0 +1,10 @@
+import DataList from "@components/DataList";
+import "@styles/containers/projects.scss";
+
+const ProjectsContainer = () => {
+  return (
+    <div>ProjectsContainer</div>
+  )
+};
+
+export default ProjectsContainer;
