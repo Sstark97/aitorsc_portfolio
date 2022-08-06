2b6d98af555bff2a726cbe2df81a23920bd4d7b9
Sstark97 <aitorscinfo@gmail.com>
2022-08-06 11:05:39 +0100

    Remove some  route

--- src/const.ts
+++ src/const.ts
@@ -5,7 +5,6 @@ import { AiFillLinkedin } from "react-icons/ai";
 export const ROUTES: RouteNav[] = [
   { route: "/", route_name: "Home" },
   { route: "/skills", route_name: "Mis Skills" },
-  { route: "/work", route_name: "Experiencia Laboral" },
   { route: "/projects", route_name: "Proyectos" },
   { route: "/contact", route_name: "Contacto" },
 ];
