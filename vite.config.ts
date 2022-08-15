636ecab39a8a8f4e6147a256983b7be3da0538f4
Sstark97 <aitorscinfo@gmail.com>
2022-08-15 17:10:26 +0100

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
-const getAlliases = () => {
-  return {
+const getAlliases = (mode) => {
+  const development = {
     '@assets': resolve(__dirname, './src/assets'),
     '@components': resolve(__dirname, './src/components'),
     '@containers': resolve(__dirname, './src/containers'),
@@ -12,6 +12,18 @@ const getAlliases = () => {
     '@styles': resolve(__dirname, './src/styles'),
     '@api': resolve(__dirname, './src/api'),
   }
+
+  const production = {
+    '@assets': resolve(__dirname, '/opt/build/repo/src/assets'),
+    '@components': resolve(__dirname, '/opt/build/repo/src/components'),
+    '@containers': resolve(__dirname, '/opt/build/repo/src/containers'),
+    '@pages': resolve(__dirname, '/opt/build/repo/src/pages'),
+    '@routes': resolve(__dirname, '/opt/build/repo/src/routes'),
+    '@styles': resolve(__dirname, '/opt/build/repo/src/styles'),
+    '@api': resolve(__dirname, '/opt/build/repo/src/api'),
+  }
+
+  return mode === 'development' ? development : production
 }
 
 // https://vitejs.dev/config/
@@ -20,7 +32,7 @@ export default defineConfig(({ command, mode }) => {
   const generalConfig = {
     plugins: [react()],
     resolve: {
-      alias: getAlliases(),
+      alias: getAlliases(mode),
     }
   }
 
