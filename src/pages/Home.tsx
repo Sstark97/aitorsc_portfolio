11ed2258c61511aeb287058845e1998678130bb1
Sstark97 <aitorscinfo@gmail.com>
2022-08-18 11:13:56 +0100

    Change some meta tags

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,23 +10,6 @@ const Home = () => (
         name="description"
         content="Soy Desarrollador Web Full-Stack, con el stack MERN aunque también tengo conocimientos de Angular, al que le gusta actualizarse constantemente y desarrollar mi mayor pasión, programar webapps o cualquier tipo de software."
       />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://ascinfo.netlify.app/" />
-      <meta property="og:title" content="ASC | Full Stack Developer" />
-      <meta
-        property="og:description"
-        content="Soy Desarrollador Web Full-Stack, con el stack MERN aunque también tengo conocimientos de Angular, al que le gusta actualizarse constantemente y desarrollar mi mayor pasión, programar webapps o cualquier tipo de software."
-      />
-      <meta property="og:image" content="../assets/og_img.jpeg" />
-
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://ascinfo.netlify.app/" />
-      <meta property="twitter:title" content="ASC | Full Stack Developer" />
-      <meta
-        property="twitter:description"
-        content="Soy Desarrollador Web Full-Stack, con el stack MERN aunque también tengo conocimientos de Angular, al que le gusta actualizarse constantemente y desarrollar mi mayor pasión, programar webapps o cualquier tipo de software."
-      />
-      <meta property="twitter:image" content="../assets/og_img.jpeg" />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta
         name="msapplication-TileImage"
