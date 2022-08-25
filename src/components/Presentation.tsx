15dd32863386783236a373332ca70bb41a840c4d
Sstark97 <aitorscinfo@gmail.com>
2022-08-25 13:24:47 +0100

    Add General Presentation Container

---
+++ src/components/Presentation.tsx
@@ -0,0 +1,15 @@
+
+import { cloneElement, Children, ReactElement } from "react";
+import { PresentationProps } from "../types";
+import "@styles/components/home_presentation.scss";
+
+const Presentation = ({ classContainer, children }: PresentationProps)=> {
+
+  return (
+    <section className={classContainer}>
+        {Children.toArray(children).map((child) => cloneElement(child as ReactElement<any>))}
+    </section>
+  )
+}
+
+export default Presentation;
--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -1,27 +1,55 @@
 import { useState, useEffect, memo } from "react";
+import { Link } from "react-router-dom";
 import { useGlobalContext } from "@containers/AppProvider";
+import Presentation from "@components/Presentation";
+import SocialNetworks from "@components/SocialNetworks";
 import HomePresentation from "@components/HomePresentation";
 import LoadingSpinner from "@components/LoadingSpinner";
 import { ReactComponent as Moon } from "@assets/moon.svg";
 import { ChildrenProps, AppState } from "../types";
 import "@styles/containers/home.scss";
+import "@styles/components/home_presentation.scss";
 
 const HomeContainer = memo(({ children }: ChildrenProps) => {
+  const [presentation, setPresentation] = useState<string>("");
+  const [name, setName] = useState<string>("");
+
   const { portFolio }: AppState = useGlobalContext();
 
-  useEffect(() => {}, [portFolio]);
+  useEffect(() => {
+    if (portFolio !== undefined) {
+      setPresentation(portFolio.presentation);
+      setName(portFolio.name);
+    }
+  }, [portFolio]);
 
   return (
-    <section className="home_container">
+    <Presentation
+      classContainer="home_container"
+      loading={portFolio === undefined}
+    >
       {portFolio === undefined ? (
         <LoadingSpinner />
       ) : (
         <>
-          <HomePresentation />
+          <article className="presentation_container">
+            <div className="presentation_header">
+              <p className="line__1">Hola, </p>
+              <p className="line__2">soy {name},</p>
+              <p className="line__3">Desarrollador Full Stack</p>
+            </div>
+            <div className="presentation">
+              {presentation}
+              <SocialNetworks />
+            </div>
+            <button className="presentation_btn">
+              <Link to="/contact">Contáctame</Link>
+            </button>
+          </article>
           <Moon id="moon_svg" />
         </>
       )}
-    </section>
+    </Presentation>
   );
 });
 
